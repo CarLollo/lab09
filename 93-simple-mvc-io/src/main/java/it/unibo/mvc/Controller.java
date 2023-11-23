2eb5a0dfd6bd390e8957a6e692fd9a994ab4d195
LorenzoCarletti <lorenzo.carletti3@studio.unibo.it>
2023-11-23 18:17:01 +0100

    Implementation Controller and GUI

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,35 +1,43 @@
 package it.unibo.mvc;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller implements FileController{
+    private File currentFile;
+
+    public Controller() {
+        final String filePath = System.getProperty("user.home") + System.getProperty("file.separator") + "output.txt";
+        this.currentFile = new File(filePath);
+    }
 
     @Override
     public void setFile(File f) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setFile'");
+        this.currentFile = f;
     }
 
     @Override
     public File getFile() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getFile'");
+        return this.currentFile;
     }
 
     @Override
     public String getPath() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPath'");
+        //ritorna il pathname corretto, non è ricorsivo
+        return this.currentFile.getPath();
     }
 
     @Override
     public void writeLine(String s) throws IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeLine'");
+        try (FileOutputStream fileStream = new FileOutputStream(this.getPath())) {
+            byte[] stringBytes = s.getBytes();
+            fileStream.write(stringBytes);
+        } 
     }
 
 }
--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,21 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.LayoutManager;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import java.awt.Dimension;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +23,97 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final String TITLE = "A very simple GUI application";
+    private final JFrame frame = new JFrame(TITLE);
+    private static final int PROPORTION = 2;
+    private final FileController controller = new Controller();
+    
+    public SimpleGUI() {
+        this.buildView();
+        this.display();
+    }
+
+    private void buildView()  {
+        final JPanel panel = new JPanel();
+        final JPanel elementPanel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JTextArea textArea = new JTextArea();
+        
+        //TOD0: delete
+        /*final JFileChooser fileChooser = new JFileChooser();
+        final JButton browserButton = new JButton(); 
+        browserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Qui abbiamo cliccato il bottone
+                final int chooseResult = fileChooser.showSaveDialog(elementPanel);
+                if(chooseResult == JFileChooser.APPROVE_OPTION) {
+                    SimpleGUI.this.controller.setFile(fileChooser.getSelectedFile());
+                    textArea.setText(SimpleGUI.this.controller.getPath());
+                } else if( chooseResult == JFileChooser.CANCEL_OPTION) {
+                    JOptionPane.showMessageDialog(elementPanel, "Errore"); // verify
+                }
+
+            }
+        });*/
+
+
+
+
+        
+        final JButton button = new JButton("Save");
+
+        elementPanel.setLayout(new BoxLayout(elementPanel, BoxLayout.Y_AXIS));
+        elementPanel.add(textArea);
+        elementPanel.add(button);
+        panel.add(elementPanel, BorderLayout.CENTER);
+        frame.setContentPane(panel);
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = textArea.getText();
+                try {
+                    SimpleGUI.this.controller.writeLine(s);
+                } catch(IOException ex) {
+                    System.out.println("Problema!");
+                }
+            }
+            
+        });
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI();
+    }
+
+    private void display() {
+        /*
+         * Make the frame one fifth the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected. In order to deal coherently with multimonitor
+         * setups, other facilities exist (see the Java documentation about this
+         * issue). It is MUCH better than manually specify the size of a window
+         * in pixel: it takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        //01.01
+        frame.pack();
+        /*
+         * OK, ready to push the frame onscreen
+         */
+        frame.setVisible(true);
+    }
 
 }
