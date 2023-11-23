37bd3ace5060329d82010c7c14bb9427293d5fad
LorenzoCarletti <lorenzo.carletti3@studio.unibo.it>
2023-11-23 18:57:01 +0100

    Simple Controller Implementation

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,18 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    void setNext(String s);
+
+    String getNext();
+
+    List<String> getHistory();
+
+    void printString();
 
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+    
+    private List<String> list = new ArrayList<>();
+
+    @Override
+    public void setNext(String s) {
+        this.list.add(s);
+    }
+
+    @Override
+    public String getNext() {
+        return this.list.get(this.list.size()-1);
+    }
+
+    @Override
+    public List<String> getHistory() {
+       return List.copyOf(this.list);
+    }
+
+    @Override
+    public void printString() {
+        System.out.println(this.getNext());
+    }
 
 }
